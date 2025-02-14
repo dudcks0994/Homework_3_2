6d0d47bf331fafcd38f0392f3bcc90007c660b99
김영찬 <jetty@gim-yeongchan-ui-MacBookPro.local>
2025-02-14 11:57:10 +0900

    dd

--- test/Lottery.t.sol
+++ test/Lottery.t.sol
@@ -22,28 +22,28 @@ contract LotteryTest is Test {
 
     function testInsufficientFunds1() public {
         vm.expectRevert();
-        lottery.buy(0); // buy할 때 송금이 이루어져야 함
+        lottery.buy(0);
     }
 
     function testInsufficientFunds2() public {
-        vm.expectRevert(); // buy에서 음수가 안되게 해야하나 언더플로가 안되게 해야하나?
+        vm.expectRevert();
         lottery.buy{value: 0.1 ether - 1}(0);
     }
 
     function testInsufficientFunds3() public {
-        vm.expectRevert(); // ether와 int 계산이 어떻게 이뤄지는지 확인
+        vm.expectRevert();
         lottery.buy{value: 0.1 ether + 1}(0);
     }
 
     function testNoDuplicate() public {
         lottery.buy{value: 0.1 ether}(0);
         vm.expectRevert();
-        lottery.buy{value: 0.1 ether}(0); //중복구입을 막는다??
+        lottery.buy{value: 0.1 ether}(0);
     }
 
     function testSellPhaseFullLength() public {
         lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours - 1); // 누군가 구입을 하면 그때부터 페이즈가 24시간내에서 진행되는것으로 보임, 기준시간은 블럭 타임스탬프
+        vm.warp(block.timestamp + 24 hours - 1);
         vm.prank(address(1));
         lottery.buy{value: 0.1 ether}(0);
     }
@@ -58,7 +58,7 @@ contract LotteryTest is Test {
 
     function testNoDrawDuringSellPhase() public {
         lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours - 1); // 말그대로 페이즈 진행 내에는 추첨금지
+        vm.warp(block.timestamp + 24 hours - 1);
         vm.expectRevert();
         lottery.draw();
     }
@@ -67,13 +67,13 @@ contract LotteryTest is Test {
         lottery.buy{value: 0.1 ether}(0);
         vm.warp(block.timestamp + 24 hours - 1);
         vm.expectRevert();
-        lottery.claim(); // 페이즈 진행 중이니까 보상도 못꺼내감
+        lottery.claim();
     }
 
     function testDraw() public {
         lottery.buy{value: 0.1 ether}(0);
         vm.warp(block.timestamp + 24 hours);
-        lottery.draw(); 
+        lottery.draw();
     }
 
     function getNextWinningNumber() private returns (uint16) {
@@ -81,19 +81,18 @@ contract LotteryTest is Test {
         lottery.buy{value: 0.1 ether}(0);
         vm.warp(block.timestamp + 24 hours);
         lottery.draw();
-        uint16 winningNumber = lottery.winningNumber(); // 당첨숫자 변수
+        uint16 winningNumber = lottery.winningNumber();
         vm.revertTo(snapshotId);
         return winningNumber;
     }
 
     function testClaimOnWin() public {
         uint16 winningNumber = getNextWinningNumber();
-        lottery.buy{value: 0.1 ether}(winningNumber);  // buy의 인자는 추첨 번호인듯
-        vm.warp(block.timestamp + 24 hours);
+        lottery.buy{value: 0.1 ether}(winningNumber); vm.warp(block.timestamp + 24 hours);
         uint256 expectedPayout = address(lottery).balance;
         lottery.draw();
         lottery.claim();
-        assertEq(received_msg_value, expectedPayout); //당첨자가 모인거 전부 들고가는듯
+        assertEq(received_msg_value, expectedPayout);
     }
 
     function testNoClaimOnLose() public {
@@ -101,7 +100,7 @@ contract LotteryTest is Test {
         lottery.buy{value: 0.1 ether}(winningNumber + 1); vm.warp(block.timestamp + 24 hours);
         lottery.draw();
         lottery.claim();
-        assertEq(received_msg_value, 0);  // 클레임이 못맞췃다고 리버트같은거 시키지않는듯
+        assertEq(received_msg_value, 0);
     }
 
     function testNoDrawDuringClaimPhase() public {
@@ -110,7 +109,7 @@ contract LotteryTest is Test {
         lottery.draw();
         lottery.claim();
         vm.expectRevert();
-        lottery.draw();   // 페이즈 끝나고 새로 풀이 열리기 전에는 드로우 리버트
+        lottery.draw();
     }
 
     function testRollover() public {
@@ -122,7 +121,7 @@ contract LotteryTest is Test {
         winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber); vm.warp(block.timestamp + 24 hours);
         lottery.draw();
-        lottery.claim();  // 못맞춘거 이월되어서 다음에 맞춘사람에게 전부 정상적으로 가는지 확인
+        lottery.claim();
         assertEq(received_msg_value, 0.2 ether);
     }
 
@@ -140,7 +139,7 @@ contract LotteryTest is Test {
 
         vm.prank(address(1));
         lottery.claim();
-        assertEq(address(1).balance, 0.1 ether);   //맞춘사람들에게 보상이 나눠서 들어오는지 확인
+        assertEq(address(1).balance, 0.1 ether);
     }
 
     receive() external payable {
